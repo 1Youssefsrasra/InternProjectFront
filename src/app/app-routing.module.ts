442724cb6d71eaf36bc61e0327c51a1e525d329c
Aymeri <aymerigermain@icloud.com>
2024-08-06 14:49:52 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './views/homepage/homepage.component';
 import { DoctorComponent } from './layouts/doctor/doctor.component';
+import { LabComponent } from './layouts/lab/lab.component';
 
 const routes: Routes = [
   {path:'', component:HomepageComponent},
   {path:'doctor', component:DoctorComponent},
+  {path: 'lab', component:LabComponent}
 
 ];
 
