9c675656beb5262bd9565a89ef1ae851b4bc72a3
Dhifallah Oussema <103474993+DhifallahOussema@users.noreply.github.com>
2024-08-06 15:50:23 +0100

    Adding homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './views/homepage/homepage.component';
-import { DoctorComponent } from './layouts/doctor/doctor.component';
 
 import { PatientComponent } from './layouts/patient/patient.component';
 
@@ -10,9 +9,8 @@ import {ResearcherComponent}from './layouts/researcher/researcher.component'
 
 const routes: Routes = [
   {path:'', component:HomepageComponent},
-  {path:'doctor', component:DoctorComponent},
 
-  {path: 'patient', component:PatientComponent}
+  {path: 'patient', component:PatientComponent},
 
 
   {path:'researcher', component:ResearcherComponent},
--- src/app/layouts/layouts.module.ts
+++ src/app/layouts/layouts.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { DoctorComponent } from './doctor/doctor.component';
 import { PatientComponent } from './patient/patient.component';
 import { ResearcherComponent } from './researcher/researcher.component';
 import { LabComponent } from './lab/lab.component';
@@ -10,7 +9,7 @@ import { ComponentsModule } from '../components/components.module';
 
 @NgModule({
   declarations: [
-    DoctorComponent,
+    
     PatientComponent,
     ResearcherComponent,
     LabComponent,
